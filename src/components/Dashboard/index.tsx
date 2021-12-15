aa24f5d26f79a878ff6530870684b2fda5920c3f
mahfantuci <maria@imaginationmedia.com>
2021-12-15 18:03:08 -0300

    add a new component - transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Sumary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./style";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
