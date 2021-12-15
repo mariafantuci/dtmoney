1f1d373e5308559202d0386aab953832c84a10a3
mahfantuci <maria@imaginationmedia.com>
2021-12-15 18:38:43 -0300

    setting MirageJS on the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,23 @@
 import { render } from "react-dom";
 import { App } from "./App";
+import { createServer } from "miragejs";
 
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 render(<App />, document.getElementById("root"));
