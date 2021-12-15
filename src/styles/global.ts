931e872feeff022f529e3777b8939a9a38811a70
mahfantuci <maria@imaginationmedia.com>
2021-12-15 16:13:34 -0300

    created a global styles using styled component

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --background: #f0f2f5;
+        --webkit-font-smoothing: antialiased;
+        --red: #E62E4D;
+        --blue: #5429CC;
+        --light-blue: #6933FF;
+        --green: #33CC95;
+        --shape: #FFFFFF;
+        --text-title: #363F5F;
+        --body-text: #969CB3;
+    }
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    /**
+    * font-size: 16px (desktop)
+    */
+    html{
+        @media (max-width: 1080px) {
+            font-size: 93.75%; /*15px*/
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%;/* 14px */
+        }
+    }
+
+    body{
+        background: var(--background);
+        --webkit-font-smoothing: antialiased;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
