02faf7b3a8d551a20553d4675fc5d81143205fdc
mahfantuci <maria@imaginationmedia.com>
2021-12-16 13:18:34 -0300

    add a new frontend component - polished and created the income/outcome bottons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from "react-modal";
-import { Container } from "./style";
+import { Container, TransitionTypeContainer } from "./style";
+
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,23 +16,35 @@ export function NewTransactionModal({
 }: NewTransactionModalProps) {
   return (
     <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
     >
-      <button 
-      type="button" 
-      onClick={onRequestClose}
-      className="react-modal-close"
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
       >
         <img src={closeImg} alt="fechar modal" />
       </button>
       <Container>
         <h2>Cadastrar Transação</h2>
-        <input placeholder="Título" />
-        <input placeholder="Valor" type="number"/>
-        <input placeholder="Categoria" />
+        <input placeholder="Título" type="text" />
+        <input placeholder="Valor" type="number" />
+
+        <TransitionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+          </button>
+        </TransitionTypeContainer>
+
+        <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
