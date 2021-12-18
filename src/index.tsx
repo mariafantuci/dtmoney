cad9c1893b2d77301fef9e95871a6ee21d43782c
mahfantuci <maria@imaginationmedia.com>
2021-12-18 16:00:23 -0300

    fixing the transactions name

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,20 @@ import { App } from "./App";
 import { createServer, Model } from "miragejs";
 
 createServer({
-
   models: {
-    transition: Model,
+    transaction: Model,
   },
 
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-        return this.schema.all('transition')
+      return this.schema.all("transaction");
     });
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
-      return schema.create('transition', data)
-    })
-
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
   },
 });
 render(<App />, document.getElementById("root"));
