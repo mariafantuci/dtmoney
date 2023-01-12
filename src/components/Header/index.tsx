3f9f7066bac121f5eae7bd5773f54300ccd26f81
mahfantuci <maria@imaginationmedia.com>
2023-01-12 19:52:01 -0300

    add radix-ui lib and configured the modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './style';
 import logo from '../../assets/logo.svg'
 export function Header(){
@@ -5,7 +7,19 @@ export function Header(){
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="two triangles one on another, both green and a white text written the site's name" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                            <Dialog.Title>Nova transação</Dialog.Title>
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     );
