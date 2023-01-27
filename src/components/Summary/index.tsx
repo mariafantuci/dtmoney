18caf677b39c07f6b08b8c618328d9c65305ade7
mahfantuci <maria@imaginationmedia.com>
2023-01-27 15:01:18 -0300

    Formatting currency and date to pt-br

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,6 +1,7 @@
 import { ArrowCircleUp, CurrencyDollar, ArrowCircleDown } from 'phosphor-react';
 import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormatter } from '../../utils/formatter';
 import { SummaryCard, SummaryContainer } from './style';
 
 export function Summary(){
@@ -28,21 +29,21 @@ export function Summary(){
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>{summary.income}</strong>
+                <strong>{priceFormatter.format(summary.income)}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#F75A68"/>
                 </header>
-                <strong>{summary.outcome}</strong>
+                <strong>{priceFormatter.format(summary.outcome)}</strong>
             </SummaryCard>
             <SummaryCard variant='green'>
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#FFFFFF"/>
                 </header>
-                <strong>{summary.total}</strong>
+                <strong>{priceFormatter.format(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { dateFormatter, priceFormatter } from '../../utils/formatter';
 import { SearchForm } from './components/SearchForm';
 import { PriceHightLight, TransactionContainer, TransactionTable } from './style';
 
@@ -20,9 +21,14 @@ export function Transactions(){
                             return(
                                 <tr key={transaction.id}>
                                     <td width="50%">{transaction.description}</td>
-                                    <td><PriceHightLight variant={transaction.type}>{transaction.price}</PriceHightLight></td>
+                                    <td>
+                                        <PriceHightLight variant={transaction.type}>
+                                            {transaction.type === 'outcome' && '- '}
+                                            { priceFormatter.format(transaction.price)}
+                                        </PriceHightLight>
+                                    </td>
                                     <td>{transaction.category}</td>
-                                    <td>{transaction.createdAt}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                                 </tr>
                             )
                         })}
