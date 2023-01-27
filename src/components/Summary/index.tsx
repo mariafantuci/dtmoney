64aab89af2c8ad6e326708d75f32adcc2808b0be
mahfantuci <maria@imaginationmedia.com>
2023-01-27 15:13:51 -0300

    creating a hook for the summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,26 +1,10 @@
 import { ArrowCircleUp, CurrencyDollar, ArrowCircleDown } from 'phosphor-react';
-import { useContext } from 'react';
-import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useSummary } from '../../hooks/useSummary';
 import { priceFormatter } from '../../utils/formatter';
 import { SummaryCard, SummaryContainer } from './style';
 
 export function Summary(){
-    const { transactions } = useContext(TransactionsContext)
-
-    const summary = transactions.reduce((acc, transaction) => {
-        if(transaction.type === 'income'){
-            acc.income += transaction.price
-            acc.total += transaction.price
-        }else{
-            acc.outcome += transaction.price
-            acc.total -= transaction.price
-        }
-        return acc;
-    }, {
-        income: 0, 
-        outcome: 0, 
-        total: 0
-    });
+    const {income, outcome, total } = useSummary()
 
     return(
         <SummaryContainer>
@@ -29,21 +13,21 @@ export function Summary(){
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>{priceFormatter.format(summary.income)}</strong>
+                <strong>{priceFormatter.format(income)}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#F75A68"/>
                 </header>
-                <strong>{priceFormatter.format(summary.outcome)}</strong>
+                <strong>{priceFormatter.format(outcome)}</strong>
             </SummaryCard>
             <SummaryCard variant='green'>
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#FFFFFF"/>
                 </header>
-                <strong>{priceFormatter.format(summary.total)}</strong>
+                <strong>{priceFormatter.format(total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
