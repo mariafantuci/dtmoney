6121ea85ebd1308a49bee5a1a9a21b341e9007e7
mahfantuci <maria@imaginationmedia.com>
2023-01-28 17:16:26 -0300

    Controlled component get the type value from the form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -63,16 +63,24 @@ export function NewTransactionModal() {
             {...register('category')}
           />
         
-          <TransactionType>
-          <TransactionTypeButton variant="income" value="income">
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton variant="outcome" value="outcome">
-              <ArrowCircleDown size={24} />
-              Saída
-            </TransactionTypeButton>
-          </TransactionType>
+          <Controller 
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return(
+                <TransactionType onValueChange={field.onChange} value={field.value}>
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
           
           <button type="submit" disabled={isSubmitting}>
             Cadastrar
